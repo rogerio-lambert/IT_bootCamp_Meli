73af8186f204959776e5cdb32b8dfeeab47a9bcb
rogerio-lambert <rogerio.lambert@mercadolivre.com>
2022-03-18 11:39:50 -0300

    initializing the 'pratica integradora 1' of day 2 spring boot

---
+++ Modulo3_Spring_boot/praticaIntegradora1/src/main/java/br/com/meli/springBoot2/praticaIntegradora1/PraticaIntegradora1Application.java
@@ -0,0 +1,13 @@
+package br.com.meli.springBoot2.praticaIntegradora1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaIntegradora1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaIntegradora1Application.class, args);
+	}
+
+}
---
+++ Modulo3_Spring_boot/praticaIntegradora1/src/test/java/br/com/meli/springBoot2/praticaIntegradora1/PraticaIntegradora1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.springBoot2.praticaIntegradora1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaIntegradora1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
