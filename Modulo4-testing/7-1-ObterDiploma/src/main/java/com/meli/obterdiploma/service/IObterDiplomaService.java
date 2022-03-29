74c4bff118536175330ad13736579f7857b1cb01
rogerio-lambert <rogerio.lambert@mercadolivre.com>
2022-03-29 10:05:06 -0300

    comeco exercicios de testes

--- Modulo4-testing/7-1-ObterDiploma/src/main/java/com/meli/obterdiploma/service/IObterDiplomaService.java
+++
@@ -1,8 +0,0 @@
-package com.meli.obterdiploma.service;
-
-import com.meli.obterdiploma.model.StudentDTO;
-
-public interface IObterDiplomaService {
-
-    StudentDTO analyzeScores(StudentDTO rq);
-}
