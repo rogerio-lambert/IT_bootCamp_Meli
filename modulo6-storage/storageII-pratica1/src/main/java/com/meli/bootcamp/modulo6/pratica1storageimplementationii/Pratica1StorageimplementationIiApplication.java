6d16201f287c93e3efcd9eb9c3b5745476f21aa8
rogerio-lambert <rogerio.lambert@mercadolivre.com>
2022-04-08 12:54:33 -0300

    add start files of 'pratica integradora1' of storage II

---
+++ modulo6-storage/storageII-pratica1/src/main/java/com/meli/bootcamp/modulo6/pratica1storageimplementationii/Pratica1StorageimplementationIiApplication.java
@@ -0,0 +1,13 @@
+package com.meli.bootcamp.modulo6.pratica1storageimplementationii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica1StorageimplementationIiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pratica1StorageimplementationIiApplication.class, args);
+    }
+
+}
---
+++ modulo6-storage/storageII-pratica1/src/test/java/com/meli/bootcamp/modulo6/pratica1storageimplementationii/Pratica1StorageimplementationIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.bootcamp.modulo6.pratica1storageimplementationii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica1StorageimplementationIiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
